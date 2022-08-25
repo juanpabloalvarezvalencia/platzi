60549d4665b0f0bfbe016cfeb2a4ac18bd239172
juanpabloalvarezvalencia <juanpabloalvarezvalencia@gmail.com>
2022-08-24 20:22:05 -0500

    Ejemplo de Polimorfismo

---
+++ Curso_POO_PHP/polimorfismo/Base.php
@@ -0,0 +1,51 @@
+<?php
+
+use Admin as GlobalAdmin;
+
+abstract class Base
+{
+
+    protected $name;
+
+    private function getClassName()
+    {
+        return get_called_class();
+    }
+
+    public function login()
+    {
+        return "Mi nombre es $this->name desde la clase {$this->getClassName()}";
+    }
+}
+
+class Admin extends Base
+{
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }        
+}
+
+class User extends Base
+{
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }        
+}
+
+class Guest extends Base
+{
+    protected $name = 'Invitado';
+}
+
+
+$guest = new Guest();
+echo $guest->login();
+
+$user = new User('Juan Pablo');
+echo $user->login();
+
+$admin = new Admin('Otto');
+echo $admin->login();
+
